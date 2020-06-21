9c581e281eb7b4a2a0a27fd936a81ac2cf9e3850
magicmage6 <zheng_yu@hotmail.com>
2020-06-21 15:42:24 -0700

    add sqrt

--- class2/variable.py
+++ class2/variable.py
@@ -1,3 +1,5 @@
+import math
+
 a = 1
 print(a)
 b = a + 1
@@ -8,3 +10,15 @@ b_str = a_str + '1'
 print(b_str)
 c_str = b_str + 'abc'
 print(c_str)
+
+input1 = input('plese type a string:')
+print(input1)
+
+input2 = int(input('plesae type a number:'))
+print(input2)
+
+input3 = float(input('plesae type a float number:'))
+print(input3)
+
+x = math.sqrt(input3)
+print(x)
