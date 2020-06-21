08ad005728ddb2f71135a678337e4e7c757b5703
magicmage6 <zheng_yu@hotmail.com>
2020-06-21 15:56:35 -0700

    home work

---
+++ class2/equation.py
@@ -0,0 +1,10 @@
+print('This is a tool to solve quadratic equations.')
+
+a = float(input('Please input a:'))
+b = float(input('please input b:'))
+c = float(input('please input c:'))
+
+print('Your equation is %gx^2 + %gx + %g = 0.' % (a, b, c))
+
+print('The solutions to the equations are:')
+
