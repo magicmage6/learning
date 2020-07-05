c57f6a0b94ec64560b05575d6459bcbbe10ca6e0
magicmage6 <51105633+magicmage6@users.noreply.github.com>
2020-07-05 16:02:23 -0700

    class 6

--- class6/plot.py
+++ class6/plot.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
 
+list_x = []
+for i in range(200):
+    list_x.append(i * 0.1 - 10)
+
+list_y = []
+for x in list_x:
+    list_y.append(x ** 3)
+
+plt.plot(list_x, list_y)
+plt.xlabel('my test x label')
+plt.ylabel('whatever')
+plt.title('my function plot')
+
+plt.show()
