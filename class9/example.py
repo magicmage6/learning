5bb0ef3ee74aab8c5f83c0e193401ca73151dcf3
magicmage6 <51105633+magicmage6@users.noreply.github.com>
2020-08-01 14:57:24 -0700

    class 9

--- class9/example.py
+++ class9/example.py
@@ -1,1 +1,39 @@
+from PIL import Image
+from PIL import ImageFilter
+im = Image.open('example.jpg')
 
+im.show()
+
+print(im.format, im.size, im.mode)
+
+# thumnail
+size = (128, 128)
+im.thumbnail(size)
+
+im.show()
+
+im.save('thumbnail.jpg', 'JPEG')
+
+# rotating
+rotated = im.rotate(45)
+rotated.save('rotated.jpg', 'JPEG')
+
+rotated.show()
+
+# transpose
+transposed = im.transpose(Image.FLIP_TOP_BOTTOM)
+transposed.save('upsidedown.jpg', 'JPEG')
+
+transposed.show()
+
+#filter
+detail = im.filter(ImageFilter.DETAIL)
+detail.save('detail.jpg', 'JPEG')
+
+detail.show()
+
+#black and white
+black_white = im.convert('L')
+black_white.save('black_white.jpg', 'JPEG')
+
+black_white.show()
