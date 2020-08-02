00eabe459ec0b74d6f58f376f4a2eba861beef49
magicmage6 <51105633+magicmage6@users.noreply.github.com>
2020-08-02 15:17:39 -0700

    class 10

--- class10/example.py
+++ class10/example.py
@@ -1,1 +1,47 @@
 
+import os
+from os import path
+
+content = """
+This is an exmple.
+Anything can be here.
+"""
+
+file_name = 'example.txt'
+
+with open(file_name , 'w') as f:
+    f.write(content)
+
+new_content = """
+This is new content.
+It will replace the old one.
+"""
+
+with open(file_name, 'w') as f:
+    f.write(new_content)
+
+with open(file_name, 'a') as f:
+    f.write('A new line.')
+
+with open(file_name) as f:
+    file_content = f.read()
+
+print(file_content)
+
+if not path.exists('output'):
+    os.makedirs('output')
+
+with open('output/new.txt', 'w') as f:
+    f.write('Some thing new.')
+
+
+html_content = """
+<html>
+  <a href="http://www.google.com">Link to Google.</a>
+</html>
+"""
+
+with open('output/example.html', 'w') as f:
+    f.write(html_content)
+
+
