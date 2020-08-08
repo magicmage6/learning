5f4cee493eb58be752e5a5f77aaf710ba2e1ba7e
magicmage6 <51105633+magicmage6@users.noreply.github.com>
2020-08-08 14:42:11 -0700

    game

--- class11/game.py
+++ class11/game.py
@@ -1,1 +1,19 @@
+import pygame
 
+pygame.init()
+
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('My first game')
+
+gameover = False
+
+while not gameover:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
+
+    pygame.display.update()
+
+
+pygame.quit()
