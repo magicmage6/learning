cf8e38bf90b4154d0332e07b47defca9c34230ec
magicmage6 <51105633+magicmage6@users.noreply.github.com>
2020-08-08 17:34:02 -0700

    game4

---
+++ class11/game3.py
@@ -0,0 +1,57 @@
+import pygame
+import random
+
+
+background_color = (255, 255, 255)
+
+pygame.init()
+
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('My first game')
+clock = pygame.time.Clock()
+
+space_ship_image = pygame.image.load('spaceship.png')
+alien_image = pygame.image.load('alien.png')
+
+def show_space_ship(x, y):
+    gameDisplay.blit(space_ship_image, (x, y))
+
+def show_alien(x, y):
+    gameDisplay.blit(alien_image, (x, y))
+    
+
+gameover = False
+
+x = 400
+y = 500
+speed_x = 0
+speed_y = 0
+
+alien_x = random.randint(0, 800)
+alien_y = 0
+alien_speed = 7
+
+while not gameover:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed_x = -5
+            elif event.key == pygame.K_RIGHT:
+                speed_x = 5
+
+    gameDisplay.fill(background_color)
+
+    alien_y += alien_speed
+    x += speed_x
+    y += speed_y
+    show_space_ship(x, y)
+    show_alien(alien_x, alien_y)
+    pygame.display.update()
+    clock.tick(60)
+
+
+pygame.quit()
