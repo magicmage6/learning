fa816ef7b3ed1cd208d294571e62ad4d3ac18526
magicmage6 <51105633+magicmage6@users.noreply.github.com>
2020-08-08 17:34:36 -0700

    game2

--- class11/game2.py
+++ class11/game2.py
@@ -7,6 +7,7 @@ pygame.init()
 
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('My first game')
+clock = pygame.time.Clock()
 
 space_ship_image = pygame.image.load('spaceship.png')
 
@@ -17,7 +18,9 @@ def show_space_ship(x, y):
 gameover = False
 
 x = 400
-y = 300
+y = 500
+speed_x = 0
+speed_y = 0
 
 while not gameover:
 
@@ -27,18 +30,17 @@ while not gameover:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x -= 5
+                speed_x = -5
             elif event.key == pygame.K_RIGHT:
-                x += 5
-            elif event.key == pygame.K_UP:
-                y -= 5
-            elif event.key == pygame.K_DOWN:
-                y += 5
+                speed_x = 5
 
     gameDisplay.fill(background_color)
 
+    x += speed_x
+    y += speed_y
     show_space_ship(x, y)
     pygame.display.update()
+    clock.tick(60)
 
 
 pygame.quit()
